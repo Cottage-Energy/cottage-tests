9f43fb088d5d5f28bf948047948d0ea62fa91c83
xtcar7 <152252927+xtcar7@users.noreply.github.com>
2024-09-06 14:26:55 +0800

    updated playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
     },
     {
        name: 'Mobile Safari',
-       use: { ...devices['iPhone 13'] },
+       use: { ...devices['iPhone 12'] },
     },
 
     /* Test against branded browsers. */
