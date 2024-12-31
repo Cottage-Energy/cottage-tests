f8b88a00709f9d304423e2986734956b0e6d9340
xtcar7 <152252927+xtcar7@users.noreply.github.com>
2024-12-31 13:43:27 +0800

    tx service

--- tests/e2e_tests/move_in/workflows/transfer_service/transfer_svc_existing_cottage_user.spec.ts
+++ tests/e2e_tests/move_in/workflows/transfer_service/transfer_svc_existing_cottage_user.spec.ts
@@ -3,7 +3,6 @@ import { generateTestUserData } from '../../../../resources/fixtures/test_user';
 import { TransferServiceTestUtilities } from '../../../../resources/fixtures/transferServiceUtilities';
 import { FastmailActions } from '../../../../resources/fixtures/fastmail_actions';
 import { LinearActions } from '../../../../resources/fixtures/linear_actions';
-//import { SupabaseQueries } from '../../../resources/fixtures/database_queries';
 import * as MoveIndata from '../../../../resources/data/move_in-data.json';
 import * as PaymentData from '../../../../resources/data/payment-data.json';
 import { CleanUp } from '../../../../resources/fixtures/userCleanUp';
