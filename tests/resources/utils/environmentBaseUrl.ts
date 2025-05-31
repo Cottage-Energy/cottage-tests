0949ddc06c0e1ae9e92c9cb259c19c4a337f7745
xtcar7 <152252927+xtcar7@users.noreply.github.com>
2025-05-31 18:38:00 +0800

    updated main workflow

--- tests/resources/utils/environmentBaseUrl.ts
+++ tests/resources/utils/environmentBaseUrl.ts
@@ -7,18 +7,15 @@ export default{
         admin_api:'https://ojaryxuxdh.execute-api.us-east-1.amazonaws.com',
         home:'https://dev.publicgrid.energy',
         pa_admin:'https://dev.publicgrid.co',
-        supabse_url:'https://wzlacfmshqvjhjczytan.supabase.co'
     },
     staging:{
         admin_api:'',
         home:'https://staging.publicgrid.energy',
         pa_admin:'https://staging.publicgrid.co',
-        supabse_url:'https://wzlacfmshqvjhjczytan.supabase.co'
     },
     prod:{
         admin_api:'https://perzebrgr8.execute-api.us-east-1.amazonaws.com',
         home:'https://publicgrid.energy',
         pa_admin:'https://publicgrid.co',
-        supabse_url:'https://wzlacfmshqvjhjczytan.supabase.co'
     },
 };
--- tests/resources/utils/supabase.ts
+++ tests/resources/utils/supabase.ts
@@ -1,9 +1,5 @@
 import { SupabaseClient } from '@supabase/supabase-js';
 //import { Config } from "sst/node/config";
 import { Database } from "./database.types";
-import environmentBaseUrl from './environmentBaseUrl';
 
-const env = process.env.ENV || 'dev';
-const supabaseUrl = environmentBaseUrl[env].supabse_url;
-const supabaseApiKey = process.env.SUPABASE_API_KEY;
-export const supabase = new SupabaseClient<Database>("https://wzlacfmshqvjhjczytan.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6bGFjZm1zaHF2amhqY3p5dGFuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2MDk2MDIwOCwiZXhwIjoxOTc2NTM2MjA4fQ.2B-fu8vqPgkWuyiDxSRAP8T5pobgtc_Q1MUZa_rYYrI");
+export const supabase = new SupabaseClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_API_KEY!);
